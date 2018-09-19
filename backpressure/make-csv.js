c5cc6f23f97d87e8f14694fc7d979e2b61e58da4
Eric Guo <ericxguo@gmail.com>
2018-09-19 01:15:46 -0500

    changed back to 100k rows

--- backpressure/make-csv.js
+++ backpressure/make-csv.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const csv = fs.createWriteStream('./file.csv')
 
-for (let i = 0; i < 1e4; i++) {
+for (let i = 0; i < 1e5; i++) {
   csv.write('Eric,Guo,02/26/1990,"Oro the Snake, Tyr the Cat"\n')
 }
 
