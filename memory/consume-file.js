4a4f8696e9b33f5b75d9b0a80a475bf7064a77b8
Eric Guo <ericxguo@gmail.com>
2018-09-19 01:35:24 -0500

    added a stream to memory demo

--- memory/consume-file.js
+++ memory/consume-file.js
@@ -2,10 +2,8 @@ const fs = require('fs')
 const server = require('http').createServer()
 
 server.on('request', (req, res, next) => {
-  fs.readFile('./big-file.txt', (err, data) => {
-    if (err) console.error(err)
-    res.end(data)
-  })
+  fs.createReadStream('./big-file.txt')
+    .pipe(res)
 })
 
 server.listen(8000, () => console.log('hey the server is up'))
